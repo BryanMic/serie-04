66ae3c95eeb7fa591bac487a09b4b5aa411c21fb
BryanMic <bryan.michel.via@gmail.com>
2022-12-12 16:01:49 +0100

    Initial commit

---
+++ exe-01.js
@@ -0,0 +1,13 @@
+function calcTabl(xTabl) {
+    let somme = 0
+    for (let i = 0; i < xTabl.length; i++) {
+        somme = somme + xTabl[i];
+        
+        }
+        return somme
+
+}
+
+
+let tabl1 = [4, 5]
+console.log(calcTabl(tabl1));
